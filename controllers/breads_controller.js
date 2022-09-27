6062aaf469841e9dba5b7eda21dd22b8f1b03ad6
SSWren <sydney.wrenwick@gmail.com>
2022-09-26 20:53:30 -0400

    sending index and show data

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -3,13 +3,20 @@ const breads = express.Router()
 const Bread = require('../models/bread.js')
 
 //INDEX
-breads.get('/', (req, res)=> {
-    res.send(Bread)
+breads.get('/', (req, res) => {
+    res.render('index')
+  // res.send(Bread)
 })
 
-//SHOW
-breads.get('/:arrayIndex', (req,res)=> {
-    res.send(Bread[req.params.arrayIndex])
+// SHOW
+breads.get('/:arrayIndex', (req, res) => {
+  if (Bread[req.params.arrayIndex]) {
+    res.render('Show', {
+      bread:Bread[req.params.arrayIndex]
+    })
+  } else {
+    res.send('404')
+  }
 })
 
 module.exports = breads
