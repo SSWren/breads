ac3799ad0703da3c5c53c5a271ce6e0faa34d6bb
SSWren <sydney.wrenwick@gmail.com>
2022-09-26 21:32:47 -0400

    sending index and show data

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -4,8 +4,18 @@ const Bread = require('../models/bread.js')
 
 //INDEX
 breads.get('/', (req, res) => {
-    res.render('index')
-  // res.send(Bread)
+  res.render('Index',
+    {
+      breads: Bread,
+      title: 'Index Page'
+    }
+  )
+// res.send(Bread)
+})
+
+// NEW
+breads.get('/new', (req, res) => {
+  res.render('new')
 })
 
 // SHOW
@@ -17,6 +27,20 @@ breads.get('/:arrayIndex', (req, res) => {
   } else {
     res.send('404')
   }
+})
+
+// CREATE
+breads.post('/', (req, res) => {
+  if (!req.body.image) {
+    req.body.image = 'https://images.unsplash.com/photo-1517686469429-8bdb88b9f907?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80'
+  }
+  if(req.body.hasGluten === 'on') {
+    req.body.hasGluten = true
+  } else {
+    req.body.hasGluten = false
+  }
+  Bread.push(req.body)
+  res.redirect('/breads')
 })
 
 module.exports = breads
